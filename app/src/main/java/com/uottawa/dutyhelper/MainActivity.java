f13bf9011e3402be42314aaf5178388be8dd958e
Hamza Basrai <hamza.basrai@gmail.com>
2017-11-10 15:41:38 -0500

    Added validation, navigation view

--- app/src/main/java/com/uottawa/dutyhelper/MainActivity.java
+++ app/src/main/java/com/uottawa/dutyhelper/MainActivity.java
@@ -5,12 +5,13 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.TextInputLayout;
 import android.support.v7.app.AppCompatActivity;
+import android.text.Editable;
 import android.text.TextUtils;
-import android.util.Log;
+import android.text.TextWatcher;
+import android.util.Patterns;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
@@ -42,6 +43,8 @@ public class MainActivity extends AppCompatActivity {
         // Initializing user and password
         mEmailField = (EditText) findViewById(R.id.email_edit_text);
         mPasswordField = (EditText) findViewById(R.id.password_edit_text);
+        mEmailLayout = (TextInputLayout) findViewById(R.id.email_input_layout);
+        mPasswordLayout = (TextInputLayout) findViewById(R.id.password_input_layout);
         mLoginButton = (Button) findViewById(R.id.btn_login);
         mSignUpButton = (Button) findViewById(R.id.btn_sign_up);
 
@@ -57,18 +60,41 @@ public class MainActivity extends AppCompatActivity {
         mLoginButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                //login();
-                mEmailLayout = (TextInputLayout) findViewById(R.id.email_input_layout);
-                mPasswordLayout = (TextInputLayout) findViewById(R.id.password_input_layout);
-                String email = mEmailField.getText().toString();
-                String password = mPasswordField.getText().toString();
-                if (TextUtils.isEmpty(email)) {
-                    mEmailLayout.setError("Required Field");
-                }
+                login();
+            }
+        });
+        mEmailField.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
 
-                if (TextUtils.isEmpty(password)) {
-                    mPasswordLayout.setError("Required Field");
-                }
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                mEmailLayout.setError(null);
+                mPasswordLayout.setError(null);
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+        mPasswordField.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                mEmailLayout.setError(null);
+                mPasswordLayout.setError(null);
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
             }
         });
 
@@ -78,57 +104,50 @@ public class MainActivity extends AppCompatActivity {
     @Override
     public void onStart() {
         super.onStart();
-        // checks if user already signed in
         FirebaseUser currentUser = mAuth.getCurrentUser();
         updateUI(currentUser);
     }
 
-    //need to update this to change activities probably
     private void updateUI(FirebaseUser currentUser) {
-        Toast.makeText(this, "User Dashboard should be shown", Toast.LENGTH_SHORT).show();
-
+       Intent intent = NavigationActivity.newInstance(getApplicationContext(), currentUser);
+       startActivity(intent);
     }
 
     private void login() {
-        Toast.makeText(this, "Logging...", Toast.LENGTH_SHORT).show();
-    }
-
-    //sign in method
-    public void signIn() {
-        //Firebase sign in function
-        mAuth.signInWithEmailAndPassword(mEmailField.getText().toString(), mPasswordField.getText().toString())
-                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
-                    @Override
-                    public void onComplete(@NonNull Task<AuthResult> task) {
-                        if (task.isSuccessful()) {
-                            // Sign in success, update UI with the signed-in user's information
-                            Log.d(TAG, "signInWithEmail:success");
-                            FirebaseUser user = mAuth.getCurrentUser();
-                            updateUI(user);
-                        } else {
-                            // If sign in fails, display a message to the user.
-                            Log.w(TAG, "signInWithEmail:failure", task.getException());
-                            mPasswordField.setError("Incorrect E-mail or Password");
-                            updateUI(null);
-                        }
+        if (validateForm()) {
+            String password = mPasswordField.getText().toString();
+            String email = mEmailField.getText().toString();
+            mAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                @Override
+                public void onComplete(@NonNull Task<AuthResult> task) {
+                    if (task.isSuccessful()) {
+                        FirebaseUser user = mAuth.getCurrentUser();
+                        updateUI(user);
+                    } else {
+                        mEmailLayout.setError("Incorrect Email or Password");
+                        mPasswordLayout.setError("Incorrect Email or Password");
                     }
-                });
+                }
+            });
+        }
     }
 
-    //create an account we should maybe make an intent to a new activity to fill out all user info
-
-
-    //makes sure the inputed text is correctly inputed
     public boolean validateForm() {
-        boolean valid = true;
-
+        boolean isValid = true;
+        String password = mPasswordField.getText().toString();
         String email = mEmailField.getText().toString();
         if (TextUtils.isEmpty(email)) {
-            mEmailField.setError("Required.");
-            valid = false;
-        } else {
-            mEmailField.setError(null);
+            mEmailLayout.setError("Required Field");
+            isValid = false;
         }
-        return valid;
+        if (!Patterns.EMAIL_ADDRESS.matcher(email).matches() && !TextUtils.isEmpty(email)) {
+            mEmailLayout.setError("Invalid Email");
+            isValid = false;
+        }
+        if (TextUtils.isEmpty(password)) {
+            mPasswordLayout.setError("Required Field");
+            isValid = false;
+        }
+        return isValid;
     }
 }
--- app/src/main/java/com/uottawa/dutyhelper/SignUpActivity.java
+++ app/src/main/java/com/uottawa/dutyhelper/SignUpActivity.java
@@ -1,9 +1,13 @@
 package com.uottawa.dutyhelper;
 
-import android.support.annotation.NonNull;
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.TextInputLayout;
+import android.support.v7.app.AppCompatActivity;
+import android.text.Editable;
 import android.text.TextUtils;
+import android.text.TextWatcher;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -15,10 +19,8 @@ import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
 
-public class SignUpActivity extends AppCompatActivity  {
+public class SignUpActivity extends AppCompatActivity {
     private static final String TAG = "EmailPassword";
 
     private EditText mEmailField;
@@ -26,11 +28,14 @@ public class SignUpActivity extends AppCompatActivity  {
     private EditText mPasswordConfirmField;
     private EditText mFirstName;
     private EditText mLastName;
-    private String uid;
+
+    private TextInputLayout mEmailLayout;
+    private TextInputLayout mPasswordLayout;
+    private TextInputLayout mPasswordConfirmLayout;
+    private TextInputLayout mFirstNameLayout;
+    private TextInputLayout mLastNameLayout;
 
     private FirebaseAuth mAuth;
-    private FirebaseDatabase mFirebaseDatabase;
-    private DatabaseReference mRef;
 
     private Button mSignUp;
 
@@ -40,16 +45,20 @@ public class SignUpActivity extends AppCompatActivity  {
         setContentView(R.layout.activity_signup);
 
         mAuth = FirebaseAuth.getInstance();
-        mFirebaseDatabase = FirebaseDatabase.getInstance();
-        mRef = mFirebaseDatabase.getReference("users");
 
         mFirstName = (EditText) findViewById(R.id.first_name_edit_text);
         mLastName = (EditText) findViewById(R.id.last_name_edit_text);
         mEmailField = (EditText) findViewById(R.id.email_edit_text);
         mPasswordField = (EditText) findViewById(R.id.password_edit_text);
         mPasswordConfirmField = (EditText) findViewById(R.id.password_confirm_edit_text);
-        mSignUp = (Button) findViewById(R.id.btn_sign_up);
 
+        mEmailLayout = (TextInputLayout) findViewById(R.id.email_input_layout);
+        mPasswordLayout = (TextInputLayout) findViewById(R.id.password_input_layout);
+        mPasswordConfirmLayout = (TextInputLayout) findViewById(R.id.password_confirm_input_layout);
+        mFirstNameLayout = (TextInputLayout) findViewById(R.id.first_name_input_layout);
+        mLastNameLayout = (TextInputLayout) findViewById(R.id.last_name_input_layout);
+
+        mSignUp = (Button) findViewById(R.id.btn_sign_up);
         mSignUp.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -57,67 +66,171 @@ public class SignUpActivity extends AppCompatActivity  {
             }
         });
 
+        mFirstName.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                mFirstNameLayout.setError(null);
+                mLastNameLayout.setError(null);
+                mEmailLayout.setError(null);
+                mPasswordLayout.setError(null);
+                mPasswordConfirmLayout.setError(null);
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+        mLastName.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                mFirstNameLayout.setError(null);
+                mLastNameLayout.setError(null);
+                mEmailLayout.setError(null);
+                mPasswordLayout.setError(null);
+                mPasswordConfirmLayout.setError(null);
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+        mEmailField.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                mFirstNameLayout.setError(null);
+                mLastNameLayout.setError(null);
+                mEmailLayout.setError(null);
+                mPasswordLayout.setError(null);
+                mPasswordConfirmLayout.setError(null);
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+        mPasswordField.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                mFirstNameLayout.setError(null);
+                mLastNameLayout.setError(null);
+                mEmailLayout.setError(null);
+                mPasswordLayout.setError(null);
+                mPasswordConfirmLayout.setError(null);
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+        mPasswordConfirmField.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                mFirstNameLayout.setError(null);
+                mLastNameLayout.setError(null);
+                mEmailLayout.setError(null);
+                mPasswordLayout.setError(null);
+                mPasswordConfirmLayout.setError(null);
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
     }
 
     private void createAccount() {
-        String email = mEmailField.getText().toString();
-        String password = mPasswordField.getText().toString();
-        Log.d(TAG, "createAccount:" + email);
-        if (!validateForm()) {
-            return;
-        }else{
-        mAuth.createUserWithEmailAndPassword(email, password)
-                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
-                    @Override
-                    public void onComplete(@NonNull Task<AuthResult> task) {
-                        if (task.isSuccessful()) {
-                            // Sign in success, update UI with the signed-in user's information
-                            Log.d(TAG, "createUserWithEmail:success");
-                            FirebaseUser user = mAuth.getCurrentUser();
-                            //updateUI(user);
-                        } else {
-                            // If sign in fails, display a message to the user.
-                            Log.w(TAG, "createUserWithEmail:failure", task.getException());
-                            Toast.makeText(SignUpActivity.this, "Authentication failed.",
-                                    Toast.LENGTH_SHORT).show();
-                        }
+        if (validateForm()) {
+            String email = mEmailField.getText().toString();
+            String password = mPasswordField.getText().toString();
+            mAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                @Override
+                public void onComplete(@NonNull Task<AuthResult> task) {
+                    if (task.isSuccessful()) {
+                        // Sign in success, update UI with the signed-in user's information
+                        Log.d(TAG, "createUserWithEmail:success");
+                        FirebaseUser user = mAuth.getCurrentUser();
+                        Intent intent = NavigationActivity.newInstance(getApplicationContext(), user);
+                        startActivity(intent);
+                    } else {
+                        // If sign in fails, display a message to the user.
+                        Log.w(TAG, "createUserWithEmail:failure", task.getException());
+                        Toast.makeText(SignUpActivity.this, "Sign up process has failed", Toast.LENGTH_SHORT).show();
                     }
-                });
-
-
-        String uid = mRef.push().getKey();
-        User user = new User(uid, mFirstName.getText().toString(), mLastName.getText().toString(),
-                email);
-            Toast.makeText(SignUpActivity.this, user.getId(),
-                    Toast.LENGTH_SHORT).show();
-        mRef.child(uid).setValue(user);
+                }
+            });
         }
     }
 
     public boolean validateForm() {
-        boolean valid = true;
+        boolean isValid = true;
 
         String email = mEmailField.getText().toString();
+        String password = mPasswordField.getText().toString();
+        String passwordConfirm = mPasswordConfirmField.getText().toString();
+        String firstName = mFirstName.getText().toString();
+        String lastName = mLastName.getText().toString();
+
         if (TextUtils.isEmpty(email)) {
-            mEmailField.setError("Required.");
-            valid = false;
+            mEmailLayout.setError("Required field");
+            isValid = false;
         }
-        if (TextUtils.isEmpty(mFirstName.getText().toString())) {
-            mFirstName.setError("Required.");
-            valid = false;
+        if (TextUtils.isEmpty(firstName)) {
+            mFirstNameLayout.setError("Required field");
+            isValid = false;
         }
-        if (TextUtils.isEmpty(mLastName.getText().toString())) {
-            mLastName.setError("Required.");
-            valid = false;
+        if (TextUtils.isEmpty(lastName)) {
+            mLastNameLayout.setError("Required field");
+            isValid = false;
+        }
+        if (TextUtils.isEmpty(password)) {
+            mPasswordLayout.setError("Required field");
+            isValid = false;
+        }
+        if (TextUtils.isEmpty(passwordConfirm)) {
+            mPasswordConfirmLayout.setError("Required field");
+            isValid = false;
+        }
+        if (password.length() < 8) {
+            mPasswordLayout.setError("Must be longer than 8 characters");
+            mPasswordConfirmLayout.setError("Must be longer than 8 characters");
+            isValid = false;
+        }
+        if (!password.equals(passwordConfirm)) {
+            mPasswordLayout.setError("Passwords do not match");
+            mPasswordConfirmLayout.setError("Passwords do not match");
+            isValid = false;
         }
 
-        if (mPasswordField.getText().toString().length() < 8) {
-            mPasswordField.setError("Must be longer than 8 characters");
-            valid = false;
-        } else {
-            mEmailField.setError(null);
-            mPasswordField.setError(null);
-        }
-        return valid;
+        return isValid;
     }
 }
