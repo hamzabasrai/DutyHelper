68f1041fdd90ba6f1fbcc686f44d2a77aba111f0
ZAID SAEED <zsaee060@uottawa.ca>
2017-11-17 15:36:56 -0500

    More changes to new task activity

--- app/src/main/java/com/uottawa/dutyhelper/NewTaskActivity.java
+++ app/src/main/java/com/uottawa/dutyhelper/NewTaskActivity.java
@@ -1,9 +1,21 @@
 package com.uottawa.dutyhelper;
 
+import android.support.design.widget.TextInputLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class NewTaskActivity extends AppCompatActivity {
+
+    private EditText mTask_Name = (EditText) findViewById(R.id.task_name);
+    private TextInputLayout mTask_Name_Layout = (TextInputLayout) findViewById(R.id.task_name_layout);
+    private EditText mTask_Description = (EditText) findViewById(R.id.task_description);
+    private TextInputLayout mTask_Description_Layout = (TextInputLayout) findViewById(R.id.task_description_layout);
+    private Button mAdd_Task = (Button) findViewById(R.id.btn_Add_Task);
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
