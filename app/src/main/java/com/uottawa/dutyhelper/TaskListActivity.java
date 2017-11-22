8bebab928f5f43264d65ef13c84b5792212d1b86
Amar Jasarbasic <amarjasarbasic@gmail.com>
2017-11-22 12:55:09 -0500

    Added the delete function to a task when you press and hold the task.

--- app/src/main/java/com/uottawa/dutyhelper/TaskListActivity.java
+++ app/src/main/java/com/uottawa/dutyhelper/TaskListActivity.java
@@ -1,12 +1,17 @@
 package com.uottawa.dutyhelper;
 
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.Menu;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ListView;
 
 import com.google.firebase.database.DataSnapshot;
@@ -46,6 +51,37 @@ public class TaskListActivity extends AppCompatActivity {
             public void onClick(View view) {
                 Intent addTaskIntent = new Intent(view.getContext(), NewTaskActivity.class);
                 startActivity(addTaskIntent);
+            }
+        });
+
+        mTasksListView.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> arg0, View arg1,
+                                           int pos, long id) {
+
+                AlertDialog.Builder builder;
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+                    builder = new AlertDialog.Builder(TaskListActivity.this, android.R.style.Theme_Material_Dialog_Alert);
+                } else {
+                    builder = new AlertDialog.Builder(TaskListActivity.this);
+                }
+                builder.setTitle("Delete Task")
+                .setMessage("Are you sure you want to delete task?")
+                .setPositiveButton(android.R.string.yes, new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                        // continue with delete
+                    }
+                })
+                .setNegativeButton(android.R.string.no, new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                        // do nothing
+                    }
+                })
+                .setIcon(android.R.drawable.ic_dialog_alert)
+                .show();
+
+
+                return true;
             }
         });
     }
