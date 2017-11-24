b8f389c20be5cf9f1d4a60ae9f46cbe3e69cab58
pfara039 <pfara039@uottawa.ca>
2017-11-24 12:04:22 -0500

    Date for new task goes to firebase but neeed to fix edit task

--- app/src/main/java/com/uottawa/dutyhelper/NewTaskActivity.java
+++ app/src/main/java/com/uottawa/dutyhelper/NewTaskActivity.java
@@ -2,10 +2,12 @@ package com.uottawa.dutyhelper;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.text.TextUtils;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.CalendarView;
 import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.Toast;
@@ -13,19 +15,33 @@ import android.widget.Toast;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+
 public class NewTaskActivity extends AppCompatActivity {
     Spinner userSpinner;
     private DatabaseReference mDatabaseTasks;
     private EditText mTaskTitle;
     private EditText mTaskDescription;
+    private CalendarView mCalendar;
+    private String date;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_task);
         mTaskTitle = (EditText) findViewById(R.id.task_name);
         mTaskDescription = (EditText) findViewById(R.id.task_description);
         mDatabaseTasks = FirebaseDatabase.getInstance().getReference("Tasks");
+        mCalendar = (CalendarView) findViewById(R.id.calendarView);
+        mCalendar.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
+            @Override
+            public void onSelectedDayChange(@NonNull CalendarView view, int year, int month, int dayOfMonth) {
+                date = String.valueOf(dayOfMonth)+"-"+ String.valueOf(month+1)+"-"+String.valueOf(year);
+            }
+        });
     }
 
     @Override
@@ -54,7 +70,7 @@ public class NewTaskActivity extends AppCompatActivity {
         String description = mTaskDescription.getText().toString().trim();
         if (!TextUtils.isEmpty(name) && !TextUtils.isEmpty(description)) {
             String id = mDatabaseTasks.push().getKey();
-            Task task = new Task(id, name, description);
+            Task task = new Task(id, name, description, date);
             mDatabaseTasks.child(id).setValue(task);
             Toast.makeText(this, "The desired task has been added", Toast.LENGTH_LONG).show();
         } else {
