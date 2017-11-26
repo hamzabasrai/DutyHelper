bf4788e59000ed70240a63e685b7d1d8f30cb617
Amar Jasarbasic <amarjasarbasic@gmail.com>
2017-11-26 15:39:57 -0500

    Added checkbox for each task to assign whether the task is completed or not.

--- app/src/main/java/com/uottawa/dutyhelper/TaskAdapter.java
+++ app/src/main/java/com/uottawa/dutyhelper/TaskAdapter.java
@@ -7,8 +7,13 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
 import android.widget.ImageView;
 import android.widget.TextView;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 import java.util.List;
 
@@ -21,6 +26,8 @@ public class TaskAdapter extends ArrayAdapter<Task> {
     private final Context mContext;
     private final List<Task> mTaskList;
 
+    private DatabaseReference mDatabaseTasks;
+
     public TaskAdapter(Context context, List<Task> taskList) {
         super(context, R.layout.task_list_item_view, taskList);
         mContext = context;
@@ -31,15 +38,32 @@ public class TaskAdapter extends ArrayAdapter<Task> {
     @Override
     public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
         View rowView = LayoutInflater.from(getContext()).inflate(R.layout.task_list_item_view, parent, false);
-        Task task = mTaskList.get(position);
+        final Task task = mTaskList.get(position);
+        mDatabaseTasks = FirebaseDatabase.getInstance().getReference("Tasks");
 
         ImageView taskIcon = (ImageView) rowView.findViewById(R.id.task_icon);
         TextView taskTitle = (TextView) rowView.findViewById(R.id.task_title);
         TextView taskDescription = (TextView) rowView.findViewById(R.id.task_description);
+        CheckBox checkBox = (CheckBox) rowView.findViewById(R.id.checkbox);
 
         taskIcon.setImageResource(R.drawable.splash_icon);
         taskTitle.setText(task.getTitle());
         taskDescription.setText(task.getDescription());
+
+        checkBox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView,boolean isChecked) {
+                String taskId = task.getId();
+                DatabaseReference dR = mDatabaseTasks.child(taskId);
+                if (isChecked){
+                    dR.child("status").setValue("complete");
+                } else {
+                    dR.child("status").setValue("incomplete");
+                }
+            }
+        }
+        );
 
         return rowView;
     }
