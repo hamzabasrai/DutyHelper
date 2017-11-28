f6aa530fcccc4d1e5aba8262c65e2610f4970af6
Hamza Basrai <hamza.basrai@gmail.com>
2017-11-27 21:34:35 -0500

    Fixed assign users dialog

--- app/src/main/java/com/uottawa/dutyhelper/NewTaskActivity.java
+++ app/src/main/java/com/uottawa/dutyhelper/NewTaskActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.text.TextUtils;
+import android.util.Log;
 import android.util.SparseBooleanArray;
 import android.view.LayoutInflater;
 import android.view.Menu;
@@ -37,10 +38,9 @@ public class NewTaskActivity extends AppCompatActivity {
     private EditText mTaskDueDate;
     private RadioGroup mTaskStatus;
     private DatePicker mDatePicker;
-    private Button mbtn_assign_user;
+    private Button mBtnAssignUser;
     private DatabaseReference mDatabaseUsers;
     private List<String> appUsers;
-    private ListView listView;
     private ArrayAdapter<String> adapter;
     private boolean[] checkedUsers;
     private ArrayList<Integer> selectedAppUsers = new ArrayList<>();
@@ -50,59 +50,21 @@ public class NewTaskActivity extends AppCompatActivity {
     private List<String> appUsersID;
     ArrayList<String> assignedUsers;
 
-    @Override
-    protected void onStart() {
-        super.onStart();
-        mDatabaseUsers.addValueEventListener(new ValueEventListener() {
-            @Override
-            public void onDataChange(DataSnapshot dataSnapshot) {
-                appUsers.clear();
-                for(DataSnapshot postSnapShot : dataSnapshot.getChildren()){
-                    User user = postSnapShot.getValue(User.class);
-                    appUsers.add(user.getFirstName() + user.getLastName());
-                    appUsersID.add(user.getId());
-
-                }
-                adapter = new ArrayAdapter<String>(NewTaskActivity.this, android.R.layout.simple_list_item_multiple_choice,appUsers);
-                listView.setChoiceMode(ListView.CHOICE_MODE_MULTIPLE);
-                listView.setAdapter(adapter);
-            }
-
-            @Override
-            public void onCancelled(DatabaseError databaseError) {
-
-            }
-        });
-
-
-    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_task);
-        final View userListView = LayoutInflater.from(getApplicationContext()).inflate(R.layout.users_item,null);
-        listView = (ListView) userListView.findViewById(R.id.users_list_view);
 
         appUsersID = new ArrayList<>();
         task = new Task();
         mDatabaseTasks = FirebaseDatabase.getInstance().getReference("Tasks");
         mTaskTitle = (EditText) findViewById(R.id.task_name);
         mTaskDescription = (EditText) findViewById(R.id.task_description);
-        mbtn_assign_user = (Button) findViewById(R.id.btn_assign_user);
+        mBtnAssignUser = (Button) findViewById(R.id.btn_assign_user);
         mDatabaseUsers = FirebaseDatabase.getInstance().getReference("users");
-        /*mDatabaseUsers.addValueEventListener(new ValueEventListener() {
-            @Override
-            public void onDataChange(DataSnapshot dataSnapshot) {
 
-            }
-
-            @Override
-            public void onCancelled(DatabaseError databaseError) {
-
-            }
-        });*/
         appUsers = new ArrayList<>();
         checkedUsers = new boolean[appUsers.size()];
         Calendar calendar = Calendar.getInstance();
@@ -112,50 +74,52 @@ public class NewTaskActivity extends AppCompatActivity {
         final int day = calendar.get(Calendar.DAY_OF_MONTH);
 
 
-        mbtn_assign_user.setOnClickListener(new View.OnClickListener() {
+        mBtnAssignUser.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
 
-                View dialogUsers = LayoutInflater.from(NewTaskActivity.this).inflate(R.layout.users_item, null);
+                View dialogAssignUser = LayoutInflater.from(NewTaskActivity.this).inflate(R.layout.dialog_assign_user, null);
+                final ListView userList = (ListView) dialogAssignUser.findViewById(R.id.users_list_view);
+
+                adapter = new ArrayAdapter<String>(NewTaskActivity.this, android.R.layout.simple_list_item_multiple_choice, appUsers);
+                userList.setChoiceMode(ListView.CHOICE_MODE_MULTIPLE);
+                userList.setAdapter(adapter);
+
                 AlertDialog.Builder builder;
                 builder = new AlertDialog.Builder(NewTaskActivity.this);
-                builder.setTitle("Assign Users").setView(listView);
-
-                builder.setCancelable(false);
-                builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialogInterface, int which) {
-                        SparseBooleanArray checkedUsers = listView.getCheckedItemPositions();
-                        assignedUsers = new ArrayList<>();
-                        for(int i = 0; i<checkedUsers.size(); i++){
-                            if(checkedUsers.get(i) == true){
-                                assignedUsers.add(appUsersID.get(i));
+                builder.setTitle("Assign Users")
+                        .setView(dialogAssignUser)
+                        .setCancelable(false)
+                        .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int which) {
+                                SparseBooleanArray checkedUsers = userList.getCheckedItemPositions();
+                                assignedUsers = new ArrayList<>();
+                                for (int i = 0; i < checkedUsers.size(); i++) {
+                                    if (checkedUsers.get(i)) {
+                                        assignedUsers.add(appUsersID.get(i));
+                                        Log.d("TAG", assignedUsers.get(i));
+                                    }
+                                }
+                                task.setAssignedUsers(assignedUsers);
                             }
-                            //Log.d("NewTaskActivity", "Users + " + appUsers.get(i) + "is Selected : " + String.valueOf(checkedUsers.get(i)) );
-                        }
-                        task.setAssignedUsers(assignedUsers);
-                    }
-                });
-
-                builder.setNegativeButton("Dismiss", new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialogInterface, int i) {
-                        dialogInterface.dismiss();
-                    }
-                });
-
-                builder.setNeutralButton("Clear All", new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialogInterface, int i) {
-                        for(int j = 0; j<checkedUsers.length; j++){
-                            checkedUsers[j] = false;
-                            selectedAppUsers.clear();
-                        }
-                    }
-                });
-                AlertDialog mDialog = builder.create();
-                mDialog.show();
-
+                        })
+//                        .setNegativeButton("Dismiss", new DialogInterface.OnClickListener() {
+//                            @Override
+//                            public void onClick(DialogInterface dialogInterface, int i) {
+//                                dialogInterface.dismiss();
+//                            }
+//                        })
+//                        .setNeutralButton("Clear All", new DialogInterface.OnClickListener() {
+//                            @Override
+//                            public void onClick(DialogInterface dialogInterface, int i) {
+//                                for (int j = 0; j < checkedUsers.length; j++) {
+//                                    checkedUsers[j] = false;
+//                                    selectedAppUsers.clear();
+//                                }
+//                            }
+//                        })
+                        .show();
             }
         });
 
@@ -164,20 +128,20 @@ public class NewTaskActivity extends AppCompatActivity {
         mTaskDueDate.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                View dialogView = LayoutInflater.from(NewTaskActivity.this).inflate(R.layout.dialog_date_picker, null);
-                mDatePicker = (DatePicker) dialogView.findViewById(R.id.date_picker);
+                View dialogDatePicker = LayoutInflater.from(NewTaskActivity.this).inflate(R.layout.dialog_date_picker, null);
+                mDatePicker = (DatePicker) dialogDatePicker.findViewById(R.id.date_picker);
                 mDatePicker.init(year, month, day, null);
 
                 AlertDialog.Builder builder;
                 builder = new AlertDialog.Builder(NewTaskActivity.this);
                 builder.setTitle("Set Due Date")
-                        .setView(dialogView)
+                        .setView(dialogDatePicker)
                         .setPositiveButton(android.R.string.yes, new DialogInterface.OnClickListener() {
                             public void onClick(DialogInterface dialog, int which) {
                                 int year = mDatePicker.getYear();
                                 int month = mDatePicker.getMonth() + 1;
                                 int day = mDatePicker.getDayOfMonth();
-                                mTaskDueDate.setText(day+"/"+month+"/"+year);
+                                mTaskDueDate.setText(day + "/" + month + "/" + year);
 
                             }
                         })
@@ -191,6 +155,29 @@ public class NewTaskActivity extends AppCompatActivity {
         });
         mTaskStatus = (RadioGroup) findViewById(R.id.status_radio_group);
     }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        mDatabaseUsers.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                appUsers.clear();
+                for (DataSnapshot postSnapShot : dataSnapshot.getChildren()) {
+                    User user = postSnapShot.getValue(User.class);
+                    appUsers.add(user.getFirstName() + user.getLastName());
+                    appUsersID.add(user.getId());
+
+                }
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+    }
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -225,6 +212,8 @@ public class NewTaskActivity extends AppCompatActivity {
 
         if (!TextUtils.isEmpty(name) && !TextUtils.isEmpty(description) && !TextUtils.isEmpty(dueDate) && !TextUtils.isEmpty(status)) {
             String id = mDatabaseTasks.push().getKey();
+
+            Log.d("TAG 2", task.getAssignedUsers().toString());
             task.setId(id);
             task.setTitle(name);
             task.setDescription(description);
@@ -233,7 +222,7 @@ public class NewTaskActivity extends AppCompatActivity {
             mDatabaseTasks.child(id).setValue(task);
 
             //assigning task to user
-            for(String UserID: assignedUsers){
+            for (String UserID : assignedUsers) {
                 ArrayList<String> toAdd = new ArrayList<>();
                 toAdd.add(id);
                 mDatabaseUsers.child(UserID).child("assignedTasks").setValue(toAdd);
